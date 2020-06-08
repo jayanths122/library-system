1972d3ee9017ecdcdc3acfe1917c0117ec181954
jayanthtydy <jayanth@tydy.it>
2020-06-08 21:48:28 +0530

    Testing 6

--- utilities/validTime.js
+++ utilities/validTime.js
@@ -5,26 +5,39 @@
  * @param {number} res Response object.
  * @return {Boolean} true || false 
 */
+const moment = require('moment-timezone');
+
 module.exports.validTime = function () {
+ 
+    // var startTime = '10:00:00';
+    // var endTime = '17:00:00';
+    
+    // const currentDate = new Date();   
+    
+    // startDate = new Date(currentDate.getTime());
+    // startDate.setHours(startTime.split(":")[0]);
+    // startDate.setMinutes(startTime.split(":")[1]);
+    // startDate.setSeconds(startTime.split(":")[2]);
+    
+    // endDate = new Date(currentDate.getTime());
+    // endDate.setHours(endTime.split(":")[0]);
+    // endDate.setMinutes(endTime.split(":")[1]);
+    // endDate.setSeconds(endTime.split(":")[2]);
+    
+    // console.log('Is valid time ?', startDate < currentDate && endDate > currentDate);
+    // console.log('Start date = ', startDate, '; Current = ', currentDate, '; End date = ', endTime);
+    
+    // var time = moment() gives you current time. no format required.
 
-    var startTime = '10:00:00';
-    var endTime = '17:00:00';
-    
-    const currentDate = new Date(new Date().toISOString())   
-    
-    startDate = new Date(currentDate.getTime());
-    startDate.setHours(startTime.split(":")[0]);
-    startDate.setMinutes(startTime.split(":")[1]);
-    startDate.setSeconds(startTime.split(":")[2]);
-    
-    endDate = new Date(currentDate.getTime());
-    endDate.setHours(endTime.split(":")[0]);
-    endDate.setMinutes(endTime.split(":")[1]);
-    endDate.setSeconds(endTime.split(":")[2]);
-    
-    console.log('Is valid time ?', startDate < currentDate && endDate > currentDate);
-    console.log('Start date = ', startDate, '; Current = ', currentDate, '; End date = ', endTime);
-    
+    // return (startDate < currentDate && endDate > currentDate);
 
-    return (startDate < currentDate && endDate > currentDate);
+    const format = 'hh:mm:ss'
+
+    const time = moment(),
+    beforeTime = moment('09:00:00', format),
+    afterTime = moment('17:00:00', format);
+
+    console.log('Start date = ', beforeTime, '; Current = ', time, '; End date = ', afterTime);
+    
+    return time.isBetween(beforeTime, afterTime);
 }
